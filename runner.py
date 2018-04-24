d9401418a41687872899ef069d04166501360673
Yogendra Tamang <48yogen@gmail.com>
2018-04-24 16:24:55 +0545

    httpProxies and runner.py add

---
+++ runner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import argparse
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+process = CrawlerProcess(get_project_settings())
+
+# 'followall' is the name of one of the spiders of the project.
+process.crawl('freeproxy')
+process.start() # the script will block here until the crawling is finished
