e0e223ba370cc2a50a489ffd53f2bbc3744d6d63
Yogendra Tamang <48yogen@gmail.com>
2018-04-24 16:33:19 +0545

    add ssl and http proxies

--- runner.py
+++ runner.py
@@ -8,4 +8,5 @@ process = CrawlerProcess(get_project_settings())
 
 # 'followall' is the name of one of the spiders of the project.
 process.crawl('freeproxy')
+process.crawl('sslproxy')
 process.start() # the script will block here until the crawling is finished
